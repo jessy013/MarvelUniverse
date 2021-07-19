0967f9c8770d6622f1251036dbce4bf981b62595
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-07-19 11:45:27 +0200

    maj
    ajout de class hero,organisation,organisation etc

---
+++ src/service/HeroService.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\service;
+
+use app\Entity\Hero;
+
+class HeroService
+{
+    function __construct()
+    {
+        
+    }
+}
+
+function getlist()
+{
+
+}
+
+function getHero()
+{
+    
+}

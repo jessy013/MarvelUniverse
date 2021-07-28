2290fa3a72a3a020e06ea7a51c229a48faf67857
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-07-28 16:20:57 +0200

    maj

--- src/Controller/HeroController.php
+++ src/Controller/HeroController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Service\HeroService;
 use App\Entity\Hero;
+use App\Form\HeroType;
 use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,57 +23,48 @@ class HeroController extends AbstractController
             'controller_name' => 'HeroController',
         ]);
     }
-/**
+    /**
      * @Route("/hero/list", name="liste_hero")
      */
     public function list(HeroService $HeroService): response
     {
-       $listeHeros =$HeroService->getlist();
-       return $this->render('hero/list.html.twig',[
-           'controller_name' => 'Herocontroller',
-            'heroList'=>$listeHeros
+        $listeHeros = $HeroService->getlist();
+        return $this->render('hero/list.html.twig', [
+            'controller_name' => 'Herocontroller',
+            'heroList' => $listeHeros
         ]);
-
     }
-    public function newHero(Request $request, HeroService $heroService):response
-        {
-            $Hero = new Hero('','',false,'','','');
-        $form = $this->createFormBuilder($Hero)
-        ->add('pseudo',TextType::class)
-        ->add('firstname',TextType::class)
-        ->add('name',TextType::class)
-        ->add('save', SubmitType::class, ['label' => 'Créer Hero'])
-            ->getForm();
-            $request = Request::createFromGlobals();
+    public function newHero(Request $request, HeroService $heroService): response
+    {
+        $hero = new Hero('', '', false, '', '', '');
+        $form = $this->createForm(HeroType::class, $hero);
+        $request = Request::createFromGlobals();
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
+        if ($form->isSubmitted() && $form->isValid()) {
             $hero = $form->getData();
             $heroService->addHero($hero);
-            return $this->render('hero/create_completed.html.twig',['hero'=>$Hero]);
-        }
-        else
-        return $this->render('hero/creer.html.twig',['formulaire'=>$form->createView()]);
-        }
+            return $this->render('hero/create_completed.html.twig', ['hero' => $hero]);
+        } else
+            return $this->render('hero/creer.html.twig', ['formulaire' => $form->createView()]);
+    }
 
-    
-        /**
- * @Route("hero/{pId}","hero_show")
- */
 
-    public function show($pId, HeroService $HeroService):Response
+    /**
+     * @Route("hero/{pId}","hero_show")
+     */
+
+    public function show($pId, HeroService $HeroService): Response
     {
         $Hero = $HeroService->getHero($pId);
-        return $this->render('hero/hero.html.twig',['hero'=>$Hero['hero']]);
+        return $this->render('hero/hero.html.twig', ['hero' => $Hero['hero']]);
     }
 
     /**
      * @Route("hero/delete/{pId}","hero_delete")
      */
-    public function delete($pId, HeroService $HeroService):Response
+    public function delete($pId, HeroService $HeroService): Response
     {
         $HeroService->deleteHero($pId);
         return $this->render('hero/delete_completed.html.twig');
     }
-
 }
